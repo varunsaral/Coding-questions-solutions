7b81496499601e9896ced3be9a01f13f61f9728c
varunsaral <42253796+varunsaral@users.noreply.github.com>
2022-01-28 09:40:07 +0530

    Update binary-search.cpp

--- binary-search/binary-search.cpp
+++ binary-search/binary-search.cpp
@@ -12,7 +12,7 @@ int search(vector<int>& nums, int target) {
 //second version
 
 int search(vector<int>& nums, int target){
-  int start = 0, int end =nums.size()-1;
+  int start = 0, end =nums.size()-1;
   
   while(start<=end){
     int mid = start+(end-start)/2;
@@ -22,7 +22,7 @@ int search(vector<int>& nums, int target){
     else if(nums[mid] > target){
       end = mid-1;
     }
-    else start=mid+1
+    else start=mid+1;
   }
   return -1;
 }
